afe44c29663a5138db1d8407556b1a2b9c449baa
Daniel <daniel.palacz@gmail.com>
2024-12-01 17:35:34 +0100

    changes the way job rules are defined (adds separate file)

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ from os.path import dirname
 
 DB_FILEPATH = dirname(__file__) + "/USED_FILE_DB.csv"
 CRAWLED_JOBS_OUTPUT_FILE = dirname(__file__) + "/CRAWLED_JOBS_OUTPUT_FILE.csv"
-JOB_ROLES = ["python"]
+
+with open("JOB_ROLES_DEFINITIONS.txt", "r") as job_defs:
+    job_definitions = [job_def.strip() for job_def in job_defs.readlines()]
+
+
+JOB_ROLES = job_definitions or ["python"]
