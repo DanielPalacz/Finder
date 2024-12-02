21384f2c82a4290cc0797744c48e6ea756d30efb
Daniel <daniel.palacz@gmail.com>
2024-12-02 19:28:31 +0100

    refactors tests after SOLID-like code improvements

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
+import multiprocessing
 from os.path import dirname
 from time import sleep
 
 import pytest
 import requests
 
+from helpers import configure_logger
+from jobs import CareerLinksFetcher
 from jobs import JobScanner
+from jobs import JobsChecker
+from jobs import LinksExtractor
+from jobs import UrlFetcher
 
 
 def test_job_scanner_initiate_object():
@@ -22,6 +28,7 @@ def test_job_scanner_run_www_check_for_the_needed_jobs(
     monkeypatch.setattr("jobs.CRAWLED_JOBS_OUTPUT_FILE", "tests/TEST_RESULT_CRAWLED_JOBS_OUTPUT_FILE.csv")
 
     url_fixture_link = "http://127.0.0.1:9999/"
+    lock = multiprocessing.Lock()
 
     company_data = [
         "1",
@@ -36,7 +43,7 @@ def test_job_scanner_run_www_check_for_the_needed_jobs(
         "Borsucza,16,Kraków,30-40-408,Kraków,Polska",
     ]
 
-    JobScanner()._run_www_check_for_the_needed_jobs(url_fixture_link, ["python"], company_data)
+    JobScanner()._JobScanner__run_www_check_for_the_needed_jobs(url_fixture_link, company_data, lock)
     result_file = dirname(__file__) + "/TEST_RESULT_CRAWLED_JOBS_OUTPUT_FILE.csv"
 
     with open(result_file, "r") as file_result:
@@ -82,8 +89,8 @@ def test_job_scanner_run_not_found_job(monkeypatch, mock_db_filepath, setup_www_
             pass
 
 
-def test_job_scanner_career_keywords():
-    assert JobScanner.CAREER_KEYWORDS == [
+def test_career_links_fetcher_keywords():
+    assert CareerLinksFetcher.CAREER_KEYWORDS == [
         "career",
         "careers",
         "jobs",
@@ -118,53 +125,89 @@ def test_job_scanner_career_keywords():
         "pracy",
         "ogłoszenia",
         "ogloszenia",
+        "employment",
+        "employment-opportunities",
+        "work-with-us",
+        "join-our-team",
+        "career-opportunities",
+        "job-openings",
+        "full-time-jobs",
+        "part-time-jobs",
+        "remote-jobs",
+        "work-from-home",
+        "job-listings",
+        "vacancy",
+        "talent-acquisition",
+        "job-board",
+        "staff-recruitment",
+        "job-application",
+        "praca-zdalna",
+        "praca-online",
+        "rekrutacja-online",
+        "oferty-zatrudnienia",
+        "rekrutacja-praca",
+        "zatrudnienie",
+        "team",
+        "nasz-zespół",
+        "praca-dla-ciebie",
+        "oferty-stazowe",
+        "zatrudnimy",
+        "praca-w-zespole",
+        "dołącz-do-nas",
+        "internships",
+        "freelance-jobs",
+        "career-path",
+        "praca-it",
+        "kariera-it",
     ]
 
 
-def test_job_scanner_fetch(monkeypatch, mock_db_filepath, setup_www_page):
+def test_url_fetcher_fetch(monkeypatch, mock_db_filepath, setup_www_page):
     monkeypatch.setattr("helpers.DB_FILEPATH", mock_db_filepath)
     monkeypatch.setattr("jobs.JOB_ROLES", ["Software developer (python)"])
     monkeypatch.setattr("jobs.CRAWLED_JOBS_OUTPUT_FILE", "tests/TEST_RESULT_CRAWLED_JOBS_OUTPUT_FILE.csv")
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    url_fetcher = UrlFetcher(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    assert isinstance(www_response, requests.models.Response)
-    assert www_response.status_code == 200
+    www_response_text = url_fetcher.fetch(url_fixture_link)
+    assert isinstance(www_response_text, str)
 
     #
     # with open(mock_db_filepath, "r") as file_result:
     #     result_content = file_result.read()
 
-    assert "Firma XYZ to lider w dostarczaniu innowacyjnych" in www_response.text
+    assert "Firma XYZ to lider w dostarczaniu innowacyjnych" in www_response_text
 
 
-def test_job_scanner_fetch_non_existing_www(monkeypatch, mock_db_filepath):
+def test_url_fetcher_fetch_non_existing_www(monkeypatch, mock_db_filepath):
     monkeypatch.setattr("helpers.DB_FILEPATH", mock_db_filepath)
     monkeypatch.setattr("jobs.JOB_ROLES", ["Software developer (python)"])
     monkeypatch.setattr("jobs.CRAWLED_JOBS_OUTPUT_FILE", "tests/TEST_RESULT_CRAWLED_JOBS_OUTPUT_FILE.csv")
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    url_fetcher = UrlFetcher(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    assert www_response is None
+    www_response_text = url_fetcher.fetch(url_fixture_link)
+    assert www_response_text is None
 
 
-def test_job_scanner_fetch_ssl_issue_www(monkeypatch, mock_db_filepath, logs_directory):
+def test_url_fetcher_fetch_ssl_issue_www(monkeypatch, mock_db_filepath, logs_directory):
     def get_raising_ssl_error(url, allow_redirects=True, timeout=5):
         raise requests.exceptions.SSLError("There is SSL issue.")
 
     monkeypatch.setattr("jobs.requests.get", get_raising_ssl_error)
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    url_fetcher = UrlFetcher(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    assert www_response is None
+    www_response_text = url_fetcher.fetch(url_fixture_link)
+    assert www_response_text is None
 
-    with open(f"{logs_directory}/{job_scanner.logger.name}.log", "r") as log_file:
+    with open(f"{logs_directory}/TestLogger.log", "r") as log_file:
         for log_line in log_file:
             pass
         else:
@@ -176,29 +219,32 @@ def test_job_scanner_fetch_ssl_issue_www(monkeypatch, mock_db_filepath, logs_dir
         raise AssertionError("Incorrect log text. It should confirm that SSL issue was handled with backup http flow.")
 
 
-def test_job_scanner_fetch_value_error_www(monkeypatch, mock_db_filepath):
+def test_url_fetcher_fetch_value_error_www(monkeypatch, mock_db_filepath):
     def get_raising_ssl_error(url, allow_redirects=True, timeout=5):
         raise ValueError("There is strange value error issue.")
 
     monkeypatch.setattr("jobs.requests.get", get_raising_ssl_error)
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    url_fetcher = UrlFetcher(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    assert www_response is None
+    www_response_text = url_fetcher.fetch(url_fixture_link)
+    assert www_response_text is None
 
 
-def test_job_scanner_extract_links(monkeypatch, mock_db_filepath, setup_www_page):
+def test_links_extractor_extract_links(monkeypatch, mock_db_filepath, setup_www_page):
     # monkeypatch.setattr("helpers.DB_FILEPATH", mock_db_filepath)
     # monkeypatch.setattr("jobs.JOB_ROLES", ["Software developer (python)"])
     # monkeypatch.setattr("jobs.CRAWLED_JOBS_OUTPUT_FILE", "tests/TEST_RESULT_CRAWLED_JOBS_OUTPUT_FILE.csv")
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    url_fetcher = UrlFetcher(logger)
+    links_extractor = LinksExtractor(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
+    www_response_text = url_fetcher.fetch(url_fixture_link)
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    links = job_scanner._extract_links("http://127.0.0.1:9999/", www_response.text)
+    links = links_extractor.extract_links(url_fixture_link, www_response_text)
 
     assert links == [
         "http://127.0.0.1:9999/#about",
@@ -209,37 +255,36 @@ def test_job_scanner_extract_links(monkeypatch, mock_db_filepath, setup_www_page
     ]
 
 
-def test_job_scanner_filter_potential_career_related_links(monkeypatch, mock_db_filepath, setup_www_page):
+def test_career_links_extractor_extract_links(monkeypatch, mock_db_filepath, setup_www_page):
 
-    job_scanner = JobScanner()
+    logger = configure_logger("TestLogger")
+    career_links_fetcher = CareerLinksFetcher(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
 
-    www_response = job_scanner._fetch(url_fixture_link)
-    links = job_scanner._extract_links(url_fixture_link, www_response.text)
-    filtered_links = job_scanner._filter_potential_career_related_links(links, url_fixture_link)
-
-    assert filtered_links == ["http://127.0.0.1:9999/#careers"]
-
-
-def test_job_scanner_get_career_related_links(monkeypatch, mock_db_filepath, setup_www_page):
-    job_scanner = JobScanner()
-    url_fixture_link = "http://127.0.0.1:9999/"
-    links = job_scanner._get_career_related_links(url_fixture_link)
+    links = career_links_fetcher.get_career_links(url_fixture_link)
 
     assert links == ["http://127.0.0.1:9999/#careers"]
 
 
-def test_job_scanner_may_company_have_the_needed_jobs(monkeypatch, mock_db_filepath, setup_www_page):
-    job_scanner = JobScanner()
+def test_job_checker_may_company_have_the_needed_jobs(monkeypatch, mock_db_filepath, setup_www_page):
+    monkeypatch.setattr("jobs.JOB_ROLES", ["Software developer (python)"])
+
+    logger = configure_logger("TestLogger")
+    jobs_checker = JobsChecker(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
-    does_have_the_needed_job = job_scanner.may_company_have_the_needed_jobs(url_fixture_link, ["tester oprogramowania"])
+
+    does_have_the_needed_job = jobs_checker.may_company_have_the_needed_jobs(url_fixture_link)
 
     assert does_have_the_needed_job
 
 
-def test_job_scanner_may_company_have_the_needed_jobs_not_have(monkeypatch, mock_db_filepath, setup_www_page):
-    job_scanner = JobScanner()
+def test_job_checker_may_company_have_the_needed_jobs_not_have(monkeypatch, mock_db_filepath, setup_www_page):
+    monkeypatch.setattr("jobs.JOB_ROLES", ["devops"])
+
+    logger = configure_logger("TestLogger")
+    jobs_checker = JobsChecker(logger)
     url_fixture_link = "http://127.0.0.1:9999/"
-    does_have_the_needed_job = job_scanner.may_company_have_the_needed_jobs(url_fixture_link, ["devops"])
+    # does_have_the_needed_job = jobs_checker.may_company_have_the_needed_jobs(url_fixture_link, ["devops"])
+    does_have_the_needed_job = jobs_checker.may_company_have_the_needed_jobs(url_fixture_link)
 
     assert not does_have_the_needed_job
